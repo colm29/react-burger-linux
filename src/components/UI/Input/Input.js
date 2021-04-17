d126c275ad4bd60512e4056d373449a846787bfc
colm29 <colm29@gmail.com>
2021-04-17 19:41:26 +0100

    Add validation feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,11 +4,16 @@ import classes from './Input.module.css';
 
 const input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.inputElement];
+
+    if (props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid)
+    }
 
     switch ( props.elementType ) {
         case ( 'input' ):
             inputElement = <input
-             className={classes.inputElement} 
+             className={inputClasses.join(' ')} 
              {...props.elementConfig} value={props.value}
               onChange={props.changed} />;
             break;
